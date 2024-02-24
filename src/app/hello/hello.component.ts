45c0e762c2bc01bec0298c005076115f1ce96271
AlAzmine <114517118+AlAzmine@users.noreply.github.com>
2024-02-23 21:00:15 -0800

    Update hello.component.ts

--- src/app/hello/hello.component.ts
+++ src/app/hello/hello.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
 
 @Component({
   selector: 'app-hello',
@@ -7,6 +15,28 @@ import { Component } from '@angular/core';
   templateUrl: './hello.component.html',
   styleUrl: './hello.component.css'
 })
-export class HelloComponent {
+export class HelloComponent implements OnInit {
+  public forecasts: WeatherForecast[] = [];
+  //serverurlswaggerui
+  baseUrl = "http://localhost:5256/";
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getForecasts();
+  }
+  getForecasts() {
+    this.http.get<WeatherForecast[]>(this.baseUrl +'weatherforecast').subscribe(
+      (result) => {
+        this.forecasts = result;
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+  }
+
+ // title = 'angularappproj.client';
+
 
 }
