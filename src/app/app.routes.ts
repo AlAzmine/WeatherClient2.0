0c4ce8cdaa8d994c27b5dda6fa79d14217598436
AlAzmine <al.azmine.594@my.csun.edu>
2024-02-23 20:51:40 -0800

    navbar,countries

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { CountriesComponent } from './countries/countries.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{path: '', component: HelloComponent, pathMatch: 'full'}, 
+{path: 'countries', component:CountriesComponent }];
